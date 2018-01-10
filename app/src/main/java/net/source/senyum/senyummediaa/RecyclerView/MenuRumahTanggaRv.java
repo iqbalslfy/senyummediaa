8db974af70b87316f96283046375e3b7232bd5da
DESKTOP-NA8JGMG\SONY <muhammad.iqbal04998@gmail.com>
2018-01-10 10:45:59 +0700

    update kategori: peralatan rumah tangga

--- app/src/main/java/net/source/senyum/senyummediaa/RecyclerView/MenuRumahTanggaRv.java
+++ app/src/main/java/net/source/senyum/senyummediaa/RecyclerView/MenuRumahTanggaRv.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import net.source.senyum.senyummediaa.Activity.Pemesanan;
+import net.source.senyum.senyummediaa.Data.DataMenuProduct;
 import net.source.senyum.senyummediaa.Data.DataRT;
 import net.source.senyum.senyummediaa.FunctionClick.ItemClickListener;
 import net.source.senyum.senyummediaa.R;
@@ -23,29 +24,26 @@ import java.util.List;
  * Created by SONY on 14/12/2017.
  */
 
-class RumahTanggaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+class RumahTanggaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
     public ImageView imageView;
-    public TextView textNamaToko,textHarga,textHargaSebPotongan;
+    public TextView txtJudul, txtHarga;
     private ItemClickListener itemClickListener;
 
     public RumahTanggaViewHolder(View itemView) {
         super(itemView);
-        imageView = (ImageView)itemView.findViewById(R.id.img_toko);
-        textNamaToko = (TextView)itemView.findViewById(R.id.txtNamaToko);
-        textHarga = itemView.findViewById(R.id.txtHarga);
-        textHargaSebPotongan = itemView.findViewById(R.id.txtHargaSebPotongan);
+        imageView = itemView.findViewById(R.id.ImgMenuProduct);
+        txtJudul = itemView.findViewById(R.id.txtJudulProduct);
+        txtHarga = itemView.findViewById(R.id.txtHargaProduct);
 
         itemView.setOnClickListener(this);
-
-        strikeThroughText(textHargaSebPotongan);
     }
 
 
-    private void strikeThroughText(TextView price){
+    private void strikeThroughText(TextView price) {
         price.setPaintFlags(price.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
     }
 
-    public void setItemClickListener(ItemClickListener itemClickListener){
+    public void setItemClickListener(ItemClickListener itemClickListener) {
         this.itemClickListener = itemClickListener;
     }
 
@@ -61,32 +59,31 @@ class RumahTanggaViewHolder extends RecyclerView.ViewHolder implements View.OnCl
 }
 
 public class MenuRumahTanggaRv extends RecyclerView.Adapter<RumahTanggaViewHolder> {
-    private List<DataRT> ListRumahTangga = new ArrayList<>();
+    private List<DataMenuProduct> itemProduct = new ArrayList<>();
     private Context context;
 
-    public MenuRumahTanggaRv(List<DataRT> listRumahTangga, Context context) {
-        ListRumahTangga = listRumahTangga;
+    public MenuRumahTanggaRv(List<DataMenuProduct> itemProduct, Context context) {
+        this.itemProduct = itemProduct;
         this.context = context;
     }
 
     @Override
     public RumahTanggaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.item_stationery, parent, false);
+        View view = inflater.inflate(R.layout.item_menu_product, parent, false);
         return new RumahTanggaViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RumahTanggaViewHolder holder, int position) {
-        holder.imageView.setImageResource(ListRumahTangga.get(position).getGambar());
-        holder.textNamaToko.setText(ListRumahTangga.get(position).getNama_toko());
-        holder.textHarga.setText(ListRumahTangga.get(position).getHarga());
-        holder.textHargaSebPotongan.setText(ListRumahTangga.get(position).getHargasebpotongan());
+        holder.imageView.setImageResource(itemProduct.get(position).getGambar());
+        holder.txtJudul.setText(itemProduct.get(position).getJudul());
+        holder.txtHarga.setText(itemProduct.get(position).getHarga());
 
         holder.setItemClickListener(new ItemClickListener() {
             @Override
             public void onClick(View view, int pos, boolean isLongClick) {
-                Toast.makeText(context, ""+ ListRumahTangga.get(pos).getNama_toko(),Toast.LENGTH_SHORT).show();
+
                 context.startActivity(new Intent(context, Pemesanan.class));
             }
         });
@@ -94,6 +91,6 @@ public class MenuRumahTanggaRv extends RecyclerView.Adapter<RumahTanggaViewHolde
 
     @Override
     public int getItemCount() {
-        return ListRumahTangga.size();
+        return itemProduct.size();
     }
 }

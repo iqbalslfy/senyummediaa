604d702e865fbfd642a042777f24b00025f3832e
DESKTOP-NA8JGMG\SONY <muhammad.iqbal04998@gmail.com>
2018-01-10 09:44:21 +0700

    update

--- app/src/main/java/net/source/senyum/senyummediaa/RecyclerView/MenuElektronikRv.java
+++ app/src/main/java/net/source/senyum/senyummediaa/RecyclerView/MenuElektronikRv.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import net.source.senyum.senyummediaa.Activity.Pemesanan;
 import net.source.senyum.senyummediaa.Data.DataElektronik;
+import net.source.senyum.senyummediaa.Data.DataMenuProduct;
 import net.source.senyum.senyummediaa.FunctionClick.ItemClickListener;
 import net.source.senyum.senyummediaa.R;
 
@@ -26,19 +27,18 @@ import java.util.List;
 
 class MenuElektronikViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
     public ImageView imageView;
-    public TextView textNamaToko,textHarga,textHargaSebPotongan;
+    public TextView txtJudul, txtHarga;
     private ItemClickListener itemClickListener;
 
     public MenuElektronikViewHolder(View itemView) {
         super(itemView);
         imageView = (ImageView)itemView.findViewById(R.id.img_toko);
-        textNamaToko = (TextView)itemView.findViewById(R.id.txtNamaToko);
-        textHarga = itemView.findViewById(R.id.txtHarga);
-        textHargaSebPotongan = itemView.findViewById(R.id.txtHargaSebPotongan);
+        imageView = itemView.findViewById(R.id.ImgMenuProduct);
+        txtJudul = itemView.findViewById(R.id.txtJudulProduct);
+        txtHarga = itemView.findViewById(R.id.txtHargaProduct);
 
         itemView.setOnClickListener(this);
 
-        strikeThroughText(textHargaSebPotongan);
     }
 
     private void strikeThroughText(TextView price){
@@ -61,32 +61,31 @@ class MenuElektronikViewHolder extends RecyclerView.ViewHolder implements View.O
 }
 
 public class MenuElektronikRv extends RecyclerView.Adapter<MenuElektronikViewHolder>{
-    private List<DataElektronik> ListElektronik = new ArrayList<>();
+    private List<DataMenuProduct> itemProduct = new ArrayList<>();
     private Context context;
 
-    public MenuElektronikRv(List<DataElektronik> listElektronik, Context context) {
-        ListElektronik = listElektronik;
+
+    public MenuElektronikRv(List<DataMenuProduct> itemProduct, Context context) {
+        this.itemProduct = itemProduct;
         this.context = context;
     }
 
     @Override
     public MenuElektronikViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.item_stationery, parent, false);
+        View view = inflater.inflate(R.layout.item_menu_product, parent, false);
         return new MenuElektronikViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MenuElektronikViewHolder holder, int position) {
-        holder.imageView.setImageResource(ListElektronik.get(position).getGambar());
-        holder.textNamaToko.setText(ListElektronik.get(position).getNama_toko());
-        holder.textHarga.setText(ListElektronik.get(position).getHarga());
-        holder.textHargaSebPotongan.setText(ListElektronik.get(position).getHargasebpotongan());
+        holder.imageView.setImageResource(itemProduct.get(position).getGambar());
+        holder.txtJudul.setText(itemProduct.get(position).getJudul());
+        holder.txtHarga.setText(itemProduct.get(position).getHarga());
 
         holder.setItemClickListener(new ItemClickListener() {
             @Override
             public void onClick(View view, int pos, boolean isLongClick) {
-                Toast.makeText(context, ""+ ListElektronik.get(pos).getNama_toko(),Toast.LENGTH_SHORT).show();
                 context.startActivity(new Intent(context, Pemesanan.class));
             }
         });
@@ -94,6 +93,6 @@ public class MenuElektronikRv extends RecyclerView.Adapter<MenuElektronikViewHol
 
     @Override
     public int getItemCount() {
-        return ListElektronik.size();
+        return itemProduct.size();
     }
 }
